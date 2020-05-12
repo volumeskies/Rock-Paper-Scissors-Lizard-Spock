f6b92a7c69ef969d712991a51e96e66f1cefceee
volumeskies <krivorotovatd@gmail.com>
2020-05-12 19:45:04 +0300

    Added client logic

--- sources/client/screens/game.ts
+++ sources/client/screens/game.ts
@@ -1,21 +1,33 @@
+import { PlayerState } from "../../common/messages";
+
 /**
  * Заголовок экрана
  */
 const title = document.querySelector( 'main.game>h2' ) as HTMLHeadingElement;
 /**
- * Форма для действий игрока
+ * Массив фигур
  */
-const form = document.forms.namedItem( 'player-roll' )!;
-/**
- * Набор полей на форме
- */
-const fieldset = form.querySelector( 'fieldset' )!;
-/**
- * Поле с загаданным числом
- */
-const numberInput = form.elements.namedItem( 'number' ) as HTMLInputElement;
+const figures = document.getElementsByClassName('figures__figure') as HTMLCollection;
 
-if ( !title || !form || !fieldset || !numberInput )
+/**
+ * Контейнер жизней
+ */
+const lives = document.querySelector('.lives') as HTMLElement;
+
+/**
+ * Кнопка старт игры
+ */
+const start = document.querySelector('.button') as HTMLButtonElement;
+
+/**
+ * Состояние игрока
+ */
+const currentMove: PlayerState = {
+	lives: 3,
+	figure: ''
+}
+
+if ( !title || !figures || !lives  || !start)
 {
 	throw new Error( 'Can\'t find required elements on "game" screen' );
 }
@@ -23,44 +35,83 @@ if ( !title || !form || !fieldset || !numberInput )
 /**
  * Обработчик хода игрока
  */
-type TurnHandler = ( number: number ) => void;
+type TurnHandler = ( currentMove: PlayerState ) => void;
 
 /**
  * Обработчик хода игрока
  */
 let turnHandler: TurnHandler;
 
-form.addEventListener( 'submit', onSubmit );
+function figuresListener(): void{
+	for(let item of figures){
+		item.addEventListener('click', onFigure);
+	}
+}
 
-/**
- * Обрабатывает отправку формы
- * 
- * @param event Событие отправки
- */
-function onSubmit( event: Event ): void
-{
+function removeFiguresListener(): void{
+	for(let item of figures){
+		item.removeEventListener('click', onFigure);
+	}
+}
+
+function startListener(): void{
+	start.addEventListener('click', onStart);
+}
+
+function removeStartListener(): void{
+	start.removeEventListener('click', onStart);
+}
+
+const delay = (ms: number) => new Promise((resolve)=>setTimeout(resolve, ms));
+
+async function onStart(event: Event){
 	event.preventDefault();
-	
-	turnHandler && turnHandler( numberInput.valueAsNumber );
+	removeStartListener();
+	figuresListener();
+	await delay(5000);
+	removeFiguresListener();
+	turnHandler && turnHandler(currentMove);
+}
+
+function onFigure(event: Event): void{
+	event.preventDefault();
+	const figure = event.target as HTMLElement;
+	switch(figure.classList[0]){
+		case 'figures__rock':
+			currentMove.figure = 'rock';
+			break;
+		case 'figures__paper':
+			currentMove.figure = 'paper';
+			break;
+		case 'figures__scissors':
+			currentMove.figure = 'scissors';
+			break;
+		case 'figures__lizard':
+			currentMove.figure = 'lizard';
+			break;
+		case 'figures__spock':
+			currentMove.figure = 'spock';
+			break;
+	}
 }
 
 /**
  * Обновляет экран игры
  * 
- * @param myTurn Ход текущего игрока?
+ * @param myTurn Ход текущего игрока
  */
-function update( myTurn: boolean ): void
+function update( myTurn: PlayerState ): void
 {
 	if ( myTurn )
 	{
 		title.textContent = 'Ваш ход';
-		fieldset.disabled = false;
-		
+		start.disabled = false;
+		startListener();
 		return;
 	}
-	
 	title.textContent = 'Ход противника';
-	fieldset.disabled = true;
+	start.disabled = true;
+	removeStartListener();
 }
 
 /**
