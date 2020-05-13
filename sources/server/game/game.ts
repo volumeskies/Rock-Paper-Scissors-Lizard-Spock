43eaf2d9db48d2f824f836492d664be2c11e69c8
volumeskies <krivorotovatd@gmail.com>
2020-05-13 15:33:30 +0300

    Added game logic

--- sources/server/game/game.ts
+++ sources/server/game/game.ts
@@ -5,7 +5,7 @@ import type {
 	AnyClientMessage,
 	AnyServerMessage,
 	GameStartedMessage,
-	GameAbortedMessage,
+	GameAbortedMessage, PlayerState,
 } from '../../common/messages.js';
 
 /**
@@ -27,7 +27,9 @@ class Game
 	/**
 	 * Информация о ходах игроков
 	 */
-	private _playersState!: WeakMap<WebSocket, number | null>;
+	private _playersState!: WeakMap<WebSocket, string>;
+	
+	private _playerLives!: WeakMap<WebSocket, number>;
 	
 	/**
 	 * @param session Сессия игры, содержащая перечень соединений с игроками
@@ -74,6 +76,7 @@ class Game
 		// Обнуляем ссылки
 		this._session = null as unknown as Game['_session'];
 		this._playersState = null as unknown as Game['_playersState'];
+		this._playerLives = null as unknown as Game['_playerLives'];
 	}
 	
 	/**
@@ -81,17 +84,20 @@ class Game
 	 */
 	private _sendStartMessage(): Promise<void[]>
 	{
-		this._playersState = new WeakMap();
-		
+		this._playersState = new WeakMap<WebSocket, string>();
+		this._playerLives = new WeakMap<WebSocket, number>();
 		const data: GameStartedMessage = {
 			type: 'gameStarted',
 			myTurn: true,
+			lives: 3,
 		};
 		const promises: Promise<void>[] = [];
 		
 		for ( const player of this._session )
 		{
 			promises.push( this._sendMessage( player, data ) );
+			this._playersState.set(player, '');
+			this._playerLives.set(player, data.lives);
 			data.myTurn = false;
 		}
 		
@@ -187,7 +193,7 @@ class Game
 		switch ( message.type )
 		{
 			case 'playerRoll':
-				this._onPlayerRoll( player, message.number );
+				this._onPlayerRoll( player, message.move );
 				break;
 			
 			case 'repeatGame':
@@ -195,6 +201,10 @@ class Game
 					.catch( onError );
 				break;
 			
+			case 'startEvent':
+				this._startButton();
+				break;
+				
 			case 'incorrectRequest':
 				this._sendMessage( player, message )
 					.catch( onError );
@@ -216,6 +226,69 @@ class Game
 				break;
 		}
 	}
+
+	private _startButton(): void{
+		for ( const player of this._session )
+		{
+			this._sendMessage(
+				player,
+				{
+					type: 'startButton',
+				},
+			)
+				.catch( onError );
+		}
+	}
+	
+	/**
+	 * Проверка фигур на победу (возвращает false, если текущая фигура побеждена и true если нет)
+	 * @param currentFigure текущая фигура
+	 * @param oppositeFigure фигура противника
+	 * @private
+	 */
+	private static _figuresHandling(currentFigure: string, oppositeFigure: string): boolean{
+		switch(currentFigure){
+			case 'paper':
+				if(oppositeFigure === 'scissors' || oppositeFigure === 'lizard')
+					return false;
+				else
+					return true;
+			case 'rock':
+				if(oppositeFigure === 'paper' || oppositeFigure === 'spock')
+					return false;
+				else
+					return true;
+			case 'scissors':
+				if(oppositeFigure === 'rock' || oppositeFigure === 'spock')
+					return false;
+				else
+					return true;
+			case 'lizard':
+				if(oppositeFigure === 'scissors' || oppositeFigure === 'rock')
+					return false;
+				else
+					return true;
+			case 'spock':
+				if(oppositeFigure === 'paper' || oppositeFigure === 'lizard')
+					return false;
+				else
+					return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Проверка на проигрыш (кол-во жизней)
+	 * @param lives жизни
+	 * @private
+	 */
+	private static _checkLose(lives: number): boolean{
+		return lives === 0;
+	}
+	
+	private static _checkAnyLose(livesOne: number, livesTwo: number): boolean{
+		return livesOne === 0 || livesTwo === 0;
+	}
 	
 	/**
 	 * Обрабатывает ход игрока
@@ -223,71 +296,72 @@ class Game
 	 * @param currentPlayer Игрок, от которого поступило сообщение
 	 * @param currentPlayerNumber Число, загаданное игроком
 	 */
-	private _onPlayerRoll( currentPlayer: WebSocket, currentPlayerNumber: number ): void
+	private _onPlayerRoll( currentPlayer: WebSocket, currentMove: PlayerState ): void
 	{
-		if ( this._playersState.get( currentPlayer ) != null )
-		{
-			this._sendMessage(
-				currentPlayer,
-				{
-					type: 'incorrectRequest',
-					message: 'Not your turn',
-				},
-			)
-				.catch( onError );
-			
-			return;
-		}
-		
-		this._playersState.set( currentPlayer, currentPlayerNumber );
-		
-		let maxNumber: number = currentPlayerNumber;
-		let maxNumberPlayer: WebSocket = currentPlayer;
-		
+		this._playersState.set(currentPlayer, currentMove.figure);
+		this._playerLives.set(currentPlayer, currentMove.lives);
+		let currentFigure: string = '';
+		let oppositeFigure: string = '';
+		let lose: boolean = false;
+		let opPlayer: WebSocket = currentPlayer;
 		for ( const player of this._session )
 		{
-			const playerNumber = this._playersState.get( player );
-			
-			if ( playerNumber == null )
-			{
-				this._sendMessage(
-					player,
-					{
-						type: 'changePlayer',
-						myTurn: true,
-					},
-				)
-					.catch( onError );
+			const playerFigure = this._playersState.get( player )!;
+			if(player === currentPlayer)
+				currentFigure! = playerFigure;
+			else{
+				oppositeFigure! = playerFigure;
+				opPlayer = player;
+			}
+				
+		}
+		let currLives: number = -1;
+		let oppLives: number = -1;
+		
+		if(!oppositeFigure || !currentFigure) return;
+		else{
+			let handle: boolean = Game._figuresHandling(currentFigure, oppositeFigure);
+			currLives = handle ? this._playerLives.get(currentPlayer)! : this._playerLives.get(currentPlayer)! - 1;
+			oppLives = handle ? this._playerLives.get(opPlayer)! - 1 : this._playerLives.get(opPlayer)!;
+			if(this._playerLives.get(currentPlayer) === currLives && this._playerLives.get(opPlayer) === oppLives) return;
+			lose = Game._checkAnyLose(this._playerLives.get(opPlayer)!, this._playerLives.get(currentPlayer)!)
+			if(!lose){
 				this._sendMessage(
 					currentPlayer,
 					{
 						type: 'changePlayer',
 						myTurn: false,
+						lives: currLives,
 					},
-				)
-					.catch( onError );
-				
-				return;
+				).catch( onError );
+
+				this._sendMessage(
+					opPlayer,
+					{
+						type: 'changePlayer',
+						myTurn: true,
+						lives: oppLives,
+					},
+				).catch( onError );
+
+
+				this._playerLives.set(currentPlayer, currLives);
+				this._playerLives.set(opPlayer, oppLives);
 			}
-			
-			if ( playerNumber > maxNumber )
-			{
-				maxNumber = playerNumber;
-				maxNumberPlayer = player;
+			else{
+				for(const player of this._session){
+					this._sendMessage(
+						player,
+						{
+							type: 'gameResult',
+							lose: Game._checkLose(this._playerLives.get(player)!),
+						},
+					)
+						.catch( onError );
+				}
 			}
 		}
 		
-		for ( const player of this._session )
-		{
-			this._sendMessage(
-				player,
-				{
-					type: 'gameResult',
-					win: ( player === maxNumberPlayer ),
-				},
-			)
-				.catch( onError );
-		}
 	}
 }
 
